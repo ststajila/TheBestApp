5beb8d017041f6f86a846067183c43663fa82291
Stanislav <stazilastas@gmail.com>
2023-11-15 09:25:40 -0600

    add controller and plan cell

---
+++ TheBestApp/PlanCell.swift
@@ -0,0 +1,14 @@
+//
+//  PlanCell.swift
+//  TheBestApp
+//
+//  Created by STANISLAV STAJILA on 11/15/23.
+//
+
+import Foundation
+import UIKit
+public class PlanCell{
+    
+    @IBOutlet weak var titleOutlet: UILabel!
+    @IBOutlet weak var dateOutlet: UILabel!
+}
